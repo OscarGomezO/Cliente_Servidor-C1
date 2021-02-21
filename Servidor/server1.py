9776adba730d5e80fdfa4df014b5f40a40630108
oscar.gomez@utp.edu.co <oscar.gomez@utp.edu.co>
2021-02-21 18:46:18 -0500

    Envio de datos C a S con nombre y tamaño2

--- Servidor/server1.py
+++ Servidor/server1.py
@@ -18,18 +18,34 @@ while True:
     print("Servidor listo!")
     #print("Carpeta de archivos Server: ", path_server)
 
-    accion, *m, fsize = socket.recv_multipart()
+    #accion, *m, fsize = socket.recv_multipart()
+    accion = socket.recv()
     print(">>> Acción del cliente: ", accion)
-    print(">>> Nombre del Archivo: ", m[0])
-    
-    print("Tamaño del Archivo: ", fsize)
-    if accion == b'1':
-        
+    #print(">>> Nombre del Archivo: ", m[0])
+    #print("Tamaño del Archivo: ", fsize)
+    #fsize = int(fsize.decode('utf-8'))
+    if accion == b'ENVIAR':
+        socket.send_multipart([b'Proceso de CARGAR iniciado'])
+        *m, fsize = socket.recv_multipart()
+        #Bprogress = tqdm.tqdm(range(fsize), f"Recibiendo {m[0]}", unit="B", unit_scale=True, unit_divisor=1024)
         #filename = open("Archivos/" + m[0].decode('utf-8'), "rb")
         #sRead = filename.read(1024)
         filename = path_server + '/' + m[0].decode('utf-8')
         with open(filename, 'wb') as f:
             f.write(m[1])
+        #Bprogress.update()
         socket.send_multipart([b'>>> Archivo guardado en Server'])
+    if accion == b"DESCARGAR":
+        print(">>> Descarga en Proceso...")
+        filename = open("Archivos/" + m[0].decode('utf-8'))
+        print("Nombre de Archivo solicitado por el Cliente: ", filename)
+        #sRead = filename.read(1024)
+        bytes = ''
+        with open(filename, 'rb') as f:
+            filename = open("ArchivosCliente/" + m[0].decode('utf-8'))
+            bytes = filename.read(1024)
+            #bytes = f.read(filename)
+        socket.sned_multipart([filename.encode('utp-8'), bytes])
+        print(">>> Archivo envia al Cliente...")
     else:
         print("Acción no conocidad".format(accion))
