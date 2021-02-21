ed4c85960916d15facc155f4c1a630ba9f37c39d
oscar.gomez@utp.edu.co <oscar.gomez@utp.edu.co>
2021-02-21 15:29:26 -0500

    Prueba codigo Clase, envio de C a S

---
+++ Servidor/server1.py
@@ -0,0 +1,31 @@
+import zmq
+import sys
+import os.path
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+#host = '127.0.1.1' # Local IP
+#port = 9000
+#socket.bind((host, port))
+socket.bind("tcp://*:5555")
+#files = sys.argv[path_server]
+
+while True:
+    #path_server = os.listdir(path="/home/ozkar11/Desktop/Bloc/UTP 10mo Semestre/Arquitectura Cliente-Servidor/Talleres/C-S_Taller1/Servidor/Archivos")
+    path_server = "/home/ozkar11/Desktop/Bloc/UTP 10mo Semestre/Arquitectura Cliente-Servidor/Talleres/C-S_Taller1/Servidor/Archivos"
+    #client_recv = socket.recv(1024).decode()
+    print("Servidor listo!")
+    #print("Carpeta de archivos Server: ", path_server)
+
+    accion, *m = socket.recv_multipart()
+    print("Acción del cliente: ", accion)
+    print("Nombre del Archivo: ", m[0])
+    if accion == b'1':
+        #filename = open("Archivos/" + m[0].decode('utf-8'), "rb")
+        #sRead = filename.read(1024)
+        filename = path_server + '/' + m[0].decode('utf-8')
+        with open(filename, 'wb') as f:
+            f.write(m[1])
+        socket.send_multipart([b'>>> Archivo guardado en Server'])
+    else:
+        print("Acción no conocidad".format(accion))
--- Servidor/serverF.py
+++ Servidor/serverF.py
@@ -5,15 +5,15 @@ import sys
 import socket
 import tqdm
 
+
 sockett = socket.socket()
 
-#Direccion IP OSCAR: 25.121.168.83
-#LOCAL HOST: 25.121.168.83
-
-
-sockett.bind(('127.0.0.1', 5555))
+host = '127.0.1.1' # Local IP
+#host = '25.121.154.185' # Local REMOTA
+port = 9000
+sockett.bind((host, port))
 sockett.listen(5)
-print(f"Escuchando a traves de {'127.0.0.1'}:{'5555'}")
+print(f"Escuchando a traves de {host}:{'9000'}")
 
 #Aceptar datos enviados por el cliente
 client_socket, address = sockett.accept()
@@ -24,82 +24,95 @@ print(f"IP: {address} esta conectado...")
 Buffer_size = 1024 * 4
 
 print("Validando Comparacion...")
-client_recv = client_socket.recv(1024)
-equal_recv = client_recv.decode('utf-8')
-print(equal_recv)
+client_recv = client_socket.recv(1024).decode('utf-8')
+#equal_recv = client_recv.decode('utf-8')
+print("Selección N°: ", client_recv)
 
+path_server = os.listdir(path="/home/ozkar11/Desktop/Bloc/UTP 10mo Semestre/Arquitectura Cliente-Servidor/Talleres/C-S_Taller1/Servidor/Archivos")
+files = sys.argv[path_server]
+print("Carpeta del Servidor", files)
 
 #--------------------------ENVIAR-------------------------------------
-
-if equal_recv == "ENVIAR":
-    
-    #Recibir datos enviados por el cliente
-    received = client_socket.recv(Buffer_size).decode()
-
-    print("Recibido: " + received)
-    fname, fsize = received.split(' ')
-
-    fname = os.path.basename(fname)
-    #fsize = sys.argv[0]
-
-    print("Nombre: " + fname)
-
-    print("Tamaño:" + fsize)
-    fsize = int(fsize)
-    print(fsize)
-    print("Iniciando servidor...")
-    
-    #Barra de progreso
-    Bprogress = tqdm.tqdm(range(fsize), f"Recibiendo {fname}", unit="B", unit_scale=True, unit_divisor=1024)
-
-    with open(fname, "wb") as f:
-        while True:
-            bytes_read = client_socket.recv(Buffer_size)
-
-            if not bytes_read:
-                break
-
-            f.write(bytes_read)
-            Bprogress.update(len(bytes_read))
-
-    client_socket.close()
-    sockett.close()
-
-
-#--------------------------DESCARGAR-------------------------------------
-if equal_recv == "DESCARGAR":
-    fname = client_socket.recv(Buffer_size).decode()
-    print("filename:" + fname)
-    fsize = os.stat(fname).st_size
-    print("Size:" + str(fsize))
-    Bprogress = tqdm.tqdm(range(fsize), f"Enviando {fname}", unit="B", unit_scale=True, unit_divisor=1024)
-    client_socket.send(str(fsize).encode('utf-8'))
-    
-    with open(fname, "rb") as f:
+def menuServ():
+    while True:
         
-        while True:
-            bytes_read = f.read(Buffer_size)
+        if client_recv == "1":
+            received = client_socket.recv(Buffer_size).decode()
+            print(">>> ENVIO")
+            #Recibir datos enviados por el cliente
+            
 
-            if not bytes_read:
-                break
-            client_socket.sendall(bytes_read)
-            Bprogress.update(len(bytes_read))
+            print("Recibido: " + received)
+            fname, fsize = received.split(' ')
 
-    client_socket.close()
-    sockett.close()
+            fname = os.path.basename(fname)
+            #fsize = sys.argv[0]
 
-#--------------------------LISTAR-------------------------------------
-if equal_recv == "LISTAR":
-    path_server = os.listdir(path="/home/dimas/Escritorio/U.T.P/10.Semestre X/Arquitectura C-S/02.Tareas/01.Tarea 1 Servidor Carga y Descarga de Archivos")
-    files = []
+            print(">>> Nombre Archivo: " + fname)
 
-    for file in path_server:
-        files.append(file)
-    
-    files_str = str(files)
-    bytes_files = files_str.encode('utf-8')
-    client_socket.sendall(bytes_files)
-    print("Archivos listados...")
+            print(">>> Tamaño Archivo:" + fsize)
+            fsize = int(fsize)
+            print("****Iniciando servidor...")
 
-    client_socket.close()
-    sockett.close()
+            #----PATH----------------------
+            
+            with open(fname, "wb") as f:
+                #Barra de progreso
+                Bprogress = tqdm.tqdm(range(fsize), f"Recibiendo {fname}", unit="B", unit_scale=True, unit_divisor=1024)
+
+                while True:
+                    bytes_read = client_socket.recv(Buffer_size)
+
+                    if not bytes_read:
+                        break
+
+                    #f.write(bytes_read)
+                    bytes_read.write(path_server)
+                    Bprogress.update(len(bytes_read))
+            client_socket.close()
+            sockett.close()
+            print("Archivo recibido con Exito.")
+        client_socket.close()
+            
+"""
+        #--------------------------DESCARGAR-------------------------------------
+        if equal_recv == "2":
+            fname = client_socket.recv(Buffer_size).decode()
+            print("filename:" + fname)
+            fsize = os.stat(fname).st_size
+            print("Size:" + str(fsize))
+            Bprogress = tqdm.tqdm(range(fsize), f"Enviando {fname}", unit="B", unit_scale=True, unit_divisor=1024)
+            client_socket.send(str(fsize).encode('utf-8'))
+            
+            with open(fname, "rb") as f:
+                
+                while True:
+                    bytes_read = f.read(Buffer_size)
+
+                    if not bytes_read:
+                        break
+                    client_socket.sendall(bytes_read)
+                    Bprogress.update(len(bytes_read))
+
+            client_socket.close()
+            sockett.close()
+
+        #--------------------------LISTAR-------------------------------------
+        if equal_recv == "3":
+            path_server = os.listdir(path="/home/ozkar11/Desktop/Bloc/UTP 10mo Semestre/Arquitectura Cliente-Servidor/Talleres/C-S_Taller1/Servidor/Archivos")
+            files = []
+
+            for file in path_server:
+                files.append(file)
+            
+            files_str = str(files)
+            bytes_files = files_str.encode('utf-8')
+            client_socket.sendall(bytes_files)
+            print("Archivos listados...")
+
+            client_socket.close()
+            sockett.close()
+"""
+
+if __name__ == "__main__":
+    menuServ()
